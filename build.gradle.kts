c456ad5120fcd422f0071c5724ecf64fb4d3dd35
Stanic <stanicmucierres@gmail.com>
2020-05-16 13:53:22 -0300

    Updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,21 @@ repositories {
     jcenter()
 }
 
+kotlin {
+    sourceSets["main"].dependencies {
+        implementation(npm("react", "16.12.0"))
+        implementation(npm("react-dom", "16.12.0"))
+        implementation(npm("@material-ui/core", "^4.9.14"))
+        implementation(npm("react-hot-loader", "^4.12.20"))
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.105-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.105-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-css-js:1.0.0-pre.105-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.105-kotlin-1.3.72")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
 }
 
--- src/main/kotlin/stanic/learningfront/Main.kt
+++ src/main/kotlin/stanic/learningfront/Main.kt
@@ -1,7 +1,23 @@
 package stanic.learningfront
 
+import kotlinx.css.*
+import react.dom.h1
+import react.dom.render
+import styled.css
+import styled.styledDiv
 import kotlin.browser.document
 
 fun main() {
-    document.bgColor = "BLACK"
+    render(document.getElementById("root")) {
+        styledDiv {
+            css {
+                justifyContent = JustifyContent.center
+                textAlign = TextAlign.center
+            }
+
+            h1 {
+                +"Hello"
+            }
+        }
+    }
 }
